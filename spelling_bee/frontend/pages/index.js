d2abc9d66b5112c26a44723299ae3bbc9ff52fc9
ktank4 <kinjal.tank15@gmail.com>
2025-03-28 23:38:45 -0500

    adding nextjs

---
+++ spelling_bee/frontend/pages/index.js
@@ -0,0 +1,13 @@
+export default function Home() {
+    return (
+      <div style={{ 
+        display: 'flex', 
+        justifyContent: 'center', 
+        alignItems: 'center', 
+        height: '100vh', 
+        backgroundColor: '#f5f5f5' 
+      }}>
+        <h1>Spelling Bee</h1>
+      </div>
+    );
+  }
